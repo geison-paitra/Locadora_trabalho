00a65765e0f9861d6bcb2ad63375289836e2c77b
unknown <geison.dge@gmail.com>
2021-04-05 21:59:35 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,16 +7,28 @@ INSERT INTO marca (idmarca, descricao)
 VALUES (NULL,'McLaren');
 INSERT INTO marca (idmarca, descricao) 
 VALUES (NULL,'Bugatti');
+INSERT INTO marca (idmarca, descricao) 
+VALUES (NULL,'Volksvagen');
+INSERT INTO marca (idmarca, descricao) 
+VALUES (NULL,'DeLorean');
+
+
 
 /* inserts de modelo*/
 INSERT INTO modelo (idmodelo, id_marca, descricao) 
 VALUES (NULL,1,'458 Spider');
 INSERT INTO modelo (idmodelo, id_marca, descricao) 
-VALUES (NULL,2,'Aventador');
+VALUES (NULL,2,'Aventador LP 700-4');
 INSERT INTO modelo (idmodelo, id_marca, descricao) 
 VALUES (NULL,3,'P90');
 INSERT INTO modelo (idmodelo, id_marca, descricao) 
 VALUES (NULL,4,'Veyron 16.4');
+INSERT INTO modelo (idmodelo, id_marca, descricao) 
+VALUES (NULL,5,'Fusca');
+INSERT INTO modelo (idmodelo, id_marca, descricao) 
+VALUES (NULL,6,'DMC-12');
+
+
 
 /*insert de carro*/
 INSERT INTO carro (idcarro, id_modelo, ano, cor, descricao, observacoes)
@@ -27,43 +39,47 @@ INSERT INTO carro (idcarro, id_modelo, ano, cor, descricao, observacoes)
 VALUES (NULL,1,2019,'Laranja','Duas portas, motor V12','De 0a 100 em 3 seg');
 INSERT INTO carro (idcarro, id_modelo, ano, cor, descricao, observacoes)
 VALUES (NULL,4,2018,'Amarelo','Duas portas, motor v8',NULL);
+INSERT INTO carro (idcarro, id_modelo, ano, cor, descricao, observacoes)
+VALUES (NULL,6,1985,'Prata','Duas portas, motor PRV V6','Precisa de 1.21 Gigawatts');
 
 /*insert cliente */
 INSERT INTO cliente (idcliente, nome, email, senha) 
-VALUES (NULL, 'Joao' , 'joao@gmail.com', '12345');
+VALUES (NULL, 'Martin McFly' , 'mmcfly@gmail.com', 'DeLorean');
 INSERT INTO cliente (idcliente, nome, email, senha) 
 VALUES (NULL, 'Irineu', 'vocenaosabe@nem.eu','irineu123');
 INSERT INTO cliente (idcliente, nome, email, senha) 
-VALUES (NULL, 'Ayrton', 'senna@f1.com', 'Br421l');
+VALUES (NULL, 'Ayrton Senna', 'senna@f1.com', 'Br421l');
 INSERT INTO cliente (idcliente, nome, email, senha) 
-VALUES (NULL, 'Zeca', 'zedapadaria@hotmail.com', 'paofrances');
+VALUES (NULL, 'Zeca Silva', 'zedapadaria@hotmail.com', 'paofrances');
 INSERT INTO cliente (idcliente, nome, email, senha) 
-VALUES (NULL, 'Allan', 'allanzoka@twitch.tv', 'tijolinho');
+VALUES (NULL, 'Allan Ferreira', 'allanzoka@twitch.tv', 'tijolinho');
+INSERT INTO cliente (idcliente, nome, email, senha) 
+VALUES (NULL, 'Barney Stinson', 'stinson_awesome@outlook.com', 'legendary');
+INSERT INTO cliente (idcliente, nome, email, senha) 
+VALUES (NULL, 'Theodore Mosby', 'ted.mosby@outlook.com', 'arquitetomosby');
+INSERT INTO cliente (idcliente, nome, email, senha) 
+VALUES (NULL, 'Marshall Eriksen', 'marshmallow@outlook.com', 'lilypad');
 
 /* insert locacao */
 INSERT INTO locacao (idlocacao, id_carro, id_cliente, data_retirada, data_devolucao, valor, observacoes)
-VALUES
+VALUES (NULL, 1, 6, '2015-06-05', '2015-06-06', 2200, 'Cliente pagou adiantado');
 INSERT INTO locacao (idlocacao, id_carro, id_cliente, data_retirada, data_devolucao, valor, observacoes)
-VALUES
+VALUES (NULL,);
 INSERT INTO locacao (idlocacao, id_carro, id_cliente, data_retirada, data_devolucao, valor, observacoes)
-VALUES
+VALUES (NULL,);
 INSERT INTO locacao (idlocacao, id_carro, id_cliente, data_retirada, data_devolucao, valor, observacoes)
-VALUES
+VALUES (NULL,);
 
 /*--------------*/
 /*UPDATE*/
-
-UPDATE modelo
-SET descricao='Aventador LP 700-4'
-WHERE idmodelo = 2;
 
 UPDATE carro
 SET ano = 2020
 WHERE idcarro = 4;
 
-UPDATE cliente
-SET nome = 'Jose'
-WHERE nome = 'Zeca';
+UPDATE marca
+SET descricao = 'Volksvwagen'
+WHERE descricao = 'Volksvagen';
 
 /*--------------------*/
 /* DELETE */
